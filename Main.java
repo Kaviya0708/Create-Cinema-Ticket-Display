d56b48832cbe4108ba623898e285f0491ebe6b4c
Kaviya <732125104051.25.cse@nandhatech.org>
2026-07-03 22:05:04 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args) {
+        String movie = "Coolie";
+        int screen = 2;
+        String seat = "A15";
+
+        System.out.println("***** MOVIE TICKET *****");
+        System.out.println("Movie Name : " + movie);
+        System.out.println("Screen No. : " + screen);
+        System.out.println("Seat No.   : " + seat);
+    }
+}
